edddec947d6e251de4993a0113c1534f3c3c1ce9
Gia Huy <huy.94.2016@gmail.com>
2025-02-28 22:33:26 +0700

    fix bug header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,7 @@ import Menu from "./Menu";
 const Header = () => {
   return (
     <Container
-      maxWidth="xl"
-      style={{ backgroundColor: "#161618", position: "fixed" }}
+      style={{ backgroundColor: "#161618", position: "fixed", width: "100%" }}
     >
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
